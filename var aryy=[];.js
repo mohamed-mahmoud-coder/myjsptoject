21abf62dff5902a7d8d5a8dfb222458cc31273c8
mmoo25678-maker <mmoo25678@gmail.com>
2026-05-02 21:01:21 +0300

    qq

--- var aryy=[];.js
+++ var aryy=[];.js
@@ -12,8 +12,8 @@ console.log(Math.ceil(Math.random()*10));
 
 
 
-function multiplyTwoNum(a,b){
-console.log(num1*num2)
+function multiplyTwoNum(num1,num2){
+
 return num1 * num2
 
 };
@@ -22,5 +22,38 @@ function randomNum(){
  return Math.ceil(Math.random()*10)
   ;
 }
+//the EXP
+//multiplyTwoNum(randomNum()==>num1,randomNum()==>num2)
 
-console.log(randomNum());
+
+//console.log(randomNum());
+function const1(){
+  return 21*5;
+}
+var random1=true;
+var getNumber;
+if(random1)
+{
+getNumber=randomNum;
+
+}
+else
+{
+  getNumber=const1;
+}
+//class function
+console.log(multiplyTwoNum(getNumber(),getNumber()))
+//High order function
+console.log(multiplyTwoNum(randomNum(),const1()))
+
+
+//inPure
+function c(x) {
+  return x * Math.random();
+}
+//Pure
+function d(x) {
+  return x * 2;
+}
+
+
