18e3770213a9a5ff4c322d4f0cbc8e04a5c7d07f
mmoo25678-maker <mmoo25678@gmail.com>
2026-05-16 20:46:08 +0300

    Default Parameter

--- class.js
+++ class.js
@@ -94,3 +94,48 @@ var boxingGym = new Gym("7-22", "right corner", "left corner")
 //console.log(boxingGym.openHrs) 
 console.log(boxingGym.stationaryBike) //
 //console.log(boxingGym.treadmill) //
+
+// Default Parameter
+function withDefaultParams(number = 10) {
+    console.log('Result:', number * number)
+}
+// ex
+class NoDefaultParams {
+    constructor(num1, num2, num3, string1, bool1) {
+        this.num1 = num1;
+        this.num2 = num2;
+        this.num3 = num3;
+        this.string1 = string1;
+        this.bool1 = bool1;
+    }
+    calculate() {
+        if(this.bool1) {
+            console.log(this.string1, this.num1 + this.num2 + this.num3);
+            return;
+        }
+        return "The value of bool1 is incorrect"
+    }
+}
+
+var fail = new NoDefaultParams(1,2,3,false);
+fail.calculate(); // 'The value of bool1 is incorrect'
+
+// the right way 
+class WithDefaultParams {
+    constructor(num1 = 1, num2 = 2, num3 = 3, string1 = "Result:", bool1 = true) {
+        this.num1 = num1;
+        this.num2 = num2;
+        this.num3 = num3;
+        this.string1 = string1;
+        this.bool1 = bool1;
+    }
+    calculate() {
+        if(this.bool1) {
+            console.log(this.string1, this.num1 + this.num2 + this.num3);
+            return;
+        }
+        return "The value of bool1 is incorrect"
+    }
+}
+var better = new WithDefaultParams();
+better.calculate(); // Result: 6
