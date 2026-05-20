0f185b6f6f9ee5cddf13e1baf74cc10a57dcb379
mmoo25678-maker <mmoo25678@gmail.com>
2026-05-20 19:58:35 +0300

    Factory Function

---
+++ class3.js
@@ -0,0 +1,58 @@
+// Task 1: Code a Person class
+class Person{
+    constructor(name="Tom", age=20, energy=100) {
+        this.name = name;
+        this.age = age;
+        this.energy = energy;
+
+    }
+    sleep() {
+        this.energy += 10;
+
+        
+    }
+    doSomethingFun() {
+        this.energy -= 10;
+        
+    }
+}
+
+
+// Task 2: Code a Worker class
+class Worker extends Person{
+    constructor(name, age, energy, xp = 0, hourlyWage = 10){
+        super(name, age, energy)
+        this.xp = xp;
+        this.hourlyWage = hourlyWage;
+
+    }
+    goToWork() {
+        this.xp += 10;
+
+    }
+}
+
+
+// Task 3: Code an intern object, run methods
+function intern() {
+   let intern = new Worker(   "Bob", 21,110, 0 ,10);
+
+    
+    intern.goToWork();
+    return intern;
+    // WRITE YOUR CODE HERE - Instantiate the Worker class with the intern properties
+    // WRITE YOUR CODE HERE - Call the goToWork() method
+    // WRITE YOUR CODE HERE - Return the intern object
+}
+
+// Task 4: Code a manager object, methods
+function manager() {
+    let manager = new Worker( "Alice", 30,120, 100,30);
+    manager.doSomethingFun();
+
+    
+    return manager;
+    // WRITE YOUR CODE HERE - Instantiate the Worker class with the manager properties
+    // WRITE YOUR CODE HERE - Call the doSomethingFun() method
+    // WRITE YOUR CODE HERE - Return the manager object
+}
